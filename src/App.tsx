fee4883bf20e5b5a091ac12761a9b3e203db4cfc
ev-indrik <delfinmur@gmail.com>
2024-05-09 16:52:42 +0300

    first  jsx-structure in App

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,46 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello Dreams</h1>
+    <div className="categories-container">
+      <div className="category-container">
+        <img src="#" alt="category_img" />
+        <div className="category-body-container">
+          <h2>Hats</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <img src="#" alt="category_img" />
+        <div className="category-body-container">
+          <h2>Jackets</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <img src="#" alt="category_img" />
+        <div className="category-body-container">
+          <h2>Sneakers</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <img src="#" alt="category_img" />
+        <div className="category-body-container">
+          <h2>Women</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
+
+      <div className="category-container">
+        <img src="#" alt="category_img" />
+        <div className="category-body-container">
+          <h2>Men</h2>
+          <p>Shop now</p>
+        </div>
+      </div>
     </div>
   );
 };
