3ffa2c4482462c2c036dcd194a2f4ec4e2601125
ev-indrik <delfinmur@gmail.com>
2024-05-18 17:16:34 +0300

    Navigation html-structure corrected

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -6,10 +6,10 @@ const Navigation = () => {
   return (
     <>
       <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img src={CrwnLogo} alt="main-logo" />
+        </Link>
         <div className="nav-links-container">
-          <Link className="logo-container" to="/">
-            <img src={CrwnLogo} alt="main-logo" />
-          </Link>
           <Link className="nav-link" to="/shop">
             SHOP
           </Link>
