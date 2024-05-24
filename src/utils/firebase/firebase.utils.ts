55e40d796bce88e6a6ca48e158da1e9548ae5c58
ev-indrik <delfinmur@gmail.com>
2024-05-24 14:47:08 +0300

    finishin creating users db with firebase

--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -32,6 +32,24 @@ export const createUserDocumentFromAuth = async (userAuth: any) => {
   const userDocRef = doc(db, "users", userAuth.uid);
   //   console.log(userDocRef);
 
-  const userSnapshot = await getDoc(userDocRef);
+  const userSnapshot: any = await getDoc(userDocRef);
   console.log(userSnapshot);
+
+  // if user data doesn't exist create/set the document with user data from userAuth in my collection
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error: any) {
+      console.log("Error while creating user:", error.message);
+    }
+  }
+
+  return userDocRef;
 };
